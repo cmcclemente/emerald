448e6eadb5ac913a213f0e382f65759623fcf8d0
Christopher Clemente <4646283+cmcclemente@users.noreply.github.com>
2021-01-08 13:45:06 -0800

    add getQuote function

---
+++ main.js
@@ -0,0 +1,10 @@
+getQuote().catch((err) => {
+    console.error(err);
+});
+
+async function getQuote() {
+    const response = await fetch('http://quotes.stormconsultancy.co.uk/random.json');
+    const data = await response.json();
+    document.getElementById('quote').innerText = data.quote;
+    console.log(data.quote);
+}
