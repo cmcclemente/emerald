23c4c5fda5c26475a60338b3cdb265beb3106572
Christopher Clemente <4646283+cmcclemente@users.noreply.github.com>
2021-01-09 23:24:38 -0800

    update the function

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-getQuote().catch(err => console.error(err));
+// getQuote().catch(err => console.error(err));
 
-async function getQuote() {
-    const response = await fetch('http://quotes.stormconsultancy.co.uk/random.json');
-    const data = await response.json();
-    document.getElementById('quote').innerText = data.quote;
+// async function getQuote() {
+//     const response = await fetch('http://quotes.stormconsultancy.co.uk/random.json');
+//     const data = await response.json();
+//     document.getElementById('quote').innerText = data.quote;
+// }
+
+getQuote();
+
+function getQuote(){
+    fetch('http://quotes.stormconsultancy.co.uk/random.json')
+    .then((response)=> {
+        return response.json()
+    }).then((data)=>{
+        document.getElementById('quote').innerText = data.quote;
+    })
+    .catch(err => console.error(err));
 }
